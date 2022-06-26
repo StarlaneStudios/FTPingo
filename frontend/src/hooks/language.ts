4af04a2a00efadc8a266d2f160d73b4c436961b2
beau@yaacomm.com <beaudenheijer@gmail.com>
2022-06-26 14:24:07 +0200

    feat: upload

--- frontend/src/hooks/language.ts
+++ frontend/src/hooks/language.ts
@@ -1,14 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 export type Language<T> = { [K in keyof T]: string };
 
 export default function useLanguage<T>(locale: string, path: string): Language<T> {
 
-	const language = useRef<Language<T>>({} as Language<T>);
+	const [ language, setLanguage ] = useState<Language<T>>({} as Language<T>);
 
 	useEffect(() => {
-		
+		const file = require(`../i18n/${locale}/${path}.json`);
+        console.log(file);
 	}, [ path ]);
 
-	return language.current;
+	return language;
 };
