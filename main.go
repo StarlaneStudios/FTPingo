a31c585f627bc0a50e06174818f78404b302ce69
beau@yaacomm.com <beaudenheijer@gmail.com>
2022-07-04 19:19:52 +0200

    feat: upload

--- main.go
+++ main.go
@@ -5,19 +5,35 @@ import (
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //go:embed frontend/dist
 var assets embed.FS
 
 func main() {
+
+	// Initializing the sqlite database
+	db, _ := sql.Open("sqlite3", "./database.db")
+
+	stmt, db_error := db.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
+
+	if db_error != nil {
+		panic(db_error)
+	}
+
+	stmt.Exec()
+
 	// Create an instance of the app structure
 	app := NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:     "FTPingo",
-		Width:     1024,
+		Width:     1400,
 		Height:    768,
 		Frameless: true,
 		Assets:    assets,
